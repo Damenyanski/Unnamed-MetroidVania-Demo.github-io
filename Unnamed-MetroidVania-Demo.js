6172c8c86d53a431ad63120374fd5cee839b9ef2
Damenyanski <d.flesher@share.epsb.ca>
2026-09-17 10:19:08 -0600

    Initialize PXP variable in setup function
    Added PXP variable and initialized it in setup function.

--- Unnamed-MetroidVania-Demo.js
+++ Unnamed-MetroidVania-Demo.js
@@ -1,17 +1,24 @@
-console.log("Hello, World");
-
 int PHP;
 int PMP;
 int PX;
 int PY;
 int RMX;
 int RMY;
+int PXP;
+
+
 
 void setup(){
   PHP=10;
   PMP=5;
+  PXP=0;
+  framecount(0);
+  RMX = 0;
+  RMY = 0;
+}
+
+void draw(){
   
 }
-void draw(){}
 void keyPressed(){}
 void keyReleased(){}
